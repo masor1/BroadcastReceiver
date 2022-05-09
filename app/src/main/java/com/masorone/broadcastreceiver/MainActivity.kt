01c90d207bbfe402080b40e2eaff09fefd23d0ea
masor1 <masor.dev@gmail.com>
2022-05-09 15:51:36 +0300

    BroadcastReceiver custom

--- app/src/main/java/com/masorone/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/masorone/broadcastreceiver/MainActivity.kt
@@ -4,16 +4,30 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
+    private var clickedCount = 0
+
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            clickedCount++
+            intent = Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra(MyReceiver.CLICKED_COUNT, clickedCount)
+            }
+            sendBroadcast(intent)
+        }
+
         val intentFilter = IntentFilter().apply {
+            addAction(MyReceiver.ACTION_CLICKED)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(Intent.ACTION_BATTERY_LOW)
         }
--- app/src/main/java/com/masorone/broadcastreceiver/MyReceiver.kt
+++ app/src/main/java/com/masorone/broadcastreceiver/MyReceiver.kt
@@ -9,6 +9,10 @@ class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            ACTION_CLICKED -> {
+                val clickedCount = intent.getIntExtra(CLICKED_COUNT, 0)
+                Toast.makeText(context, "ACTION_CLICKED: $clickedCount", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(context, "ACTION_BATTERY_LOW", Toast.LENGTH_SHORT).show()
             }
@@ -22,5 +26,12 @@ class MyReceiver : BroadcastReceiver() {
                     .show()
             }
         }
+    }
+
+    companion object {
+
+        const val ACTION_CLICKED = "clicked"
+
+        const val CLICKED_COUNT = "clicked_count"
     }
 }
